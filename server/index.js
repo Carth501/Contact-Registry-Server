be2ab40e475a7e354ec762f7d850e412da968a85
Carth501 <carth501@gmail.com>
2023-07-14 16:09:31 -0400

    rewrite server functions to use GCP Firestore

--- server/index.js
+++ server/index.js
@@ -4,108 +4,91 @@ const cors = require("cors");
 const express = require("express");
 const path = require('path');
 const bodyParser = require("body-parser");
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGESENDERID,
+  appId: process.env.APPID
+};
+
+const fbapp = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.json()); //am I still using this?
 
-const jsonParser = bodyParser.json();
-
-var mysql = require('mysql2');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "ZK#9Q&mBN"
-});
-
-const testsql = `SELECT * FROM sys.records LIMIT 3`;
-
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    con.query(testsql, function (err, result) {
-      if (err) throw err;
-    });
-  }); 
-
-  const recordsTable = 'sys.records3';
+const jsonParser = bodyParser.json(); //am I still using this?
 
 app.use(express.static(path.join(__dirname, '../react-app/build')));
 
 app.use(
-    cors({
-        origin: '*',
-        methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
-        exposedHeaders: ["X-SESSION-TOKEN"]
-    })
+  cors({
+    origin: '*',
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    exposedHeaders: ["X-SESSION-TOKEN"]
+  })
 );
 
-app.post('/api/create_record3', jsonParser, function (req, res) {
-  console.log(req.body);
-  const cols = "name, email, address, zip, phone, registrationemail, registrationmail, registrationtexts, registrationcalls, electionemail, electionmail, electiontexts, electioncalls";
-  const args = [
-    req.body.name,
-    req.body.email,
-    req.body.address,
-    req.body.zip,
-    req.body.phoneNumber, 
-    req.body.registrationEmail,
-    req.body.registrationMail,
-    req.body.registrationTexts, 
-    req.body.registrationCalls, 
-    req.body.electionEmail, 
-    req.body.electionMail, 
-    req.body.electionTexts, 
-    req.body.electionCalls
-  ];
-      
-  con.query(`INSERT INTO ${recordsTable} (${cols}) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, args, function (err, result) {
-    if (err) throw err;
-    console.log("Result: " + JSON.stringify(result));
-    res.send();
-  });
+app.post('/api/create_record_GCP_Fs', async function (req, res) {
+  try {
+    const docRef = await addDoc(collection(db, "users", req.body.phoneNumber), {
+      name: req.body.name,
+      email: req.body.email,
+      address: req.body.address,
+      zip: req.body.zip,
+      phoneNumber: req.body.phoneNumber,
+      registrationEmail: req.body.registrationEmail,
+      registrationMail: req.body.registrationMail,
+      registrationTexts: req.body.registrationTexts,
+      registrationCalls: req.body.registrationCalls,
+      electionEmail: req.body.electionEmail,
+      electionMail: req.body.electionMail,
+      electionTexts: req.body.electionTexts,
+      electionCalls: req.body.electionCalls,
+      datetime: Date.now()
+    });
+    res.send(docRef);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
 });
 
-app.get('/api/data', jsonParser, function(req, res) {
-  const recordsRequest = `
-  SELECT * FROM (
-    SELECT *,if(@last_phone=phone,0,1) as new_phone_group,@last_phone:=phone
-    FROM ${recordsTable} 
-    ORDER BY phone,creationtime DESC
-  ) as t1
-  WHERE new_phone_group=1;
-  `;
-  con.query(recordsRequest, function (err, result) {
-    if (err) throw err;
-    console.log("Result: " + JSON.stringify(result));
-    res.send(result);
+app.get('/api/data', async function (req, res) {
+  const querySnapshot = await getDocs(collection(db, "users"));
+  const docs = [];
+  querySnapshot.forEach((doc) => {
+    docs.push(doc.data())
   });
+  res.send(docs);
 });
 
-app.post('/api/newCustomer', jsonParser, function (req, res) {
-  const cols = 'name, apikey';
+app.post('/api/newCustomer', async function (req, res) {
   let apikey = Math.round(Math.random() * 10000000000);
-  const args = [req.body.name, apikey];
-
-  con.query(`INSERT INTO sys.customers (${cols}) VALUES (?, ?)`, args, function (err, result) {
-    if (err) throw err;
-    console.log("Result: " + JSON.stringify(result));
-    res.send({apikey: apikey});
-  });
+  try {
+    const docRef = await addDoc(collection(db, "customer", req.body.name,), {
+      apikey: apikey,
+    });
+    res.send(docRef);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
 });
 
-app.post('/api/retrieve', jsonParser, function (req, res) {
-  const args = [req.body.uuid];
-
-  con.query(`SELECT phone FROM ${recordsTable} WHERE guid = ?`,
-  args, function (err, result) {
-    if (err) throw err;
-    res.send(result[0]);
-  })
+app.post('/api/retrieve', async function (req, res) {
+  const querySnapshot = await getDocs(collection(db, "users", req.body.phoneNumber));
+  const docs = [];
+  querySnapshot.forEach((doc) => {
+    docs.push(doc.data())
+  });
+  res.send(docs);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`);
 });
